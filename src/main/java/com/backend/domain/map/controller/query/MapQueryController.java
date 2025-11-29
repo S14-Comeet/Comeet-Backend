95385d84c5cefd9a0d191edfaeba263c90e8d4c2
seunghun <kangseunghun9927@gmail.com>
2025-11-30 00:40:40 +0900

    feat(#6): 주변 매장 조회 API 엔드포인트 추가

---
+++ src/main/java/com/backend/domain/map/controller/query/MapQueryController.java
@@ -0,0 +1,41 @@
+package com.backend.domain.map.controller.query;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.common.response.BaseResponse;
+import com.backend.common.util.ResponseUtils;
+import com.backend.domain.map.dto.request.MapBoundsReqDto;
+import com.backend.domain.map.dto.response.MapMarkersResDto;
+import com.backend.domain.map.service.query.MapQueryService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Map", description = "지도 API")
+@RestController
+@RequestMapping("/map")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+class MapQueryController {
+
+	private final MapQueryService mapQueryService;
+
+	@Operation(summary = "주변 매장 조회", description = "사용자 위치 기반으로 일정 거리 내의 매장 목록을 조회합니다.")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "200", description = "주변 매장 조회 성공"),
+		@ApiResponse(responseCode = "400", description = "잘못된 요청 파라미터")
+	})
+	@GetMapping("/markers")
+	public ResponseEntity<BaseResponse<MapMarkersResDto>> getStoresWithinDistance(
+		@ModelAttribute final MapBoundsReqDto reqDto) {
+		final MapMarkersResDto response = mapQueryService.getStoresWithinDistance(reqDto);
+		return ResponseUtils.ok(response);
+	}
+}
