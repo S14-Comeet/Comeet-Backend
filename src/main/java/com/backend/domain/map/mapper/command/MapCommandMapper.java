6cb0f0f3a103db2ec01df4448f5045c3e17a82a7
seunghun <kangseunghun9927@gmail.com>
2025-11-30 00:41:44 +0900

    feat(#6): cqrs 구조에 맞춰 파일 추가

---
+++ src/main/java/com/backend/domain/map/mapper/command/MapCommandMapper.java
@@ -0,0 +1,4 @@
+package com.backend.domain.map.mapper.command;
+
+public interface MapCommandMapper {
+}
---
+++ src/main/java/com/backend/domain/map/service/command/MapCommandServiceImpl.java
@@ -0,0 +1,8 @@
+package com.backend.domain.map.service.command;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class MapCommandServiceImpl implements MapCommandService {
+
+}
