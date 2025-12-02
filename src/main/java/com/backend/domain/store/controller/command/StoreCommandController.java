99f9fc5eb969a155dbe5d02e27d72c5d37158446
seunghun <kangseunghun9927@gmail.com>
2025-12-02 10:53:07 +0900

    feat(#8): map -> store로 단순 이전

---
+++ src/main/java/com/backend/domain/store/controller/command/StoreCommandController.java
@@ -0,0 +1,10 @@
+package com.backend.domain.store.controller.command;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/stores")
+class StoreCommandController {
+
+}
---
+++ src/main/java/com/backend/domain/store/service/command/StoreCommandService.java
@@ -0,0 +1,4 @@
+package com.backend.domain.store.service.command;
+
+public interface StoreCommandService {
+}
