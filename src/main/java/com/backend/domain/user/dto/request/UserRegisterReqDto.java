b2a1cea46ad78f82d227e1fd24d771a240f5c6bc
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-04 23:11:05 +0900

    ✨ feat(#11): CurrentUser 어노테이션 수정 및 요청 DTO에 사용자 역할 추가

--- src/main/java/com/backend/domain/user/dto/request/UserRegisterReqDto.java
+++ src/main/java/com/backend/domain/user/dto/request/UserRegisterReqDto.java
@@ -2,6 +2,8 @@ package com.backend.domain.user.dto.request;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.backend.domain.user.entity.Role;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
@@ -9,6 +11,9 @@ public record UserRegisterReqDto(
 	@Schema(description = "사용자 닉네임", example = "김싸피")
 	@NotBlank(message = "닉네임은 공백일 수 없습니다.")
 	@Length(min = 1, max = 12)
-	String nickname
+	String nickname,
+
+	@Schema(description = "사용자 역할", example = "USER")
+	Role role
 ) {
 }
