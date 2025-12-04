3b89557725b97303a051383c5f514f09ef7d4dbc
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-04 13:13:03 +0900

    🔧 chore: OAuth 리다이렉트 URI 수정

--- src/main/java/com/backend/common/auth/constants/AuthConstant.java
+++ src/main/java/com/backend/common/auth/constants/AuthConstant.java
@@ -7,5 +7,5 @@ import lombok.NoArgsConstructor;
 public final class AuthConstant {
 	public static final String AUTHORIZATION = "Authorization";
 	public static final String BEARER = "Bearer ";
-	public static final String LOCAL_OAUTH_REDIRECT_URI = "http://localhost:5173";
+	public static final String LOCAL_OAUTH_REDIRECT_URI = "http://localhost:5173/oauth/callback";
 }
