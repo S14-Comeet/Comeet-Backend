6c929a0ab395a97ed653a0fcc95ad49177669782
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-07 21:06:38 +0900

    ♻️ refactor(#14): 에러 코드 중복 수정 및 정리

--- src/main/java/com/backend/common/error/ErrorCode.java
+++ src/main/java/com/backend/common/error/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode {
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "C-001", "잘못된 요청입니다."),
 	NOT_FOUND(HttpStatus.NOT_FOUND, "C-002", "리소스를 찾을 수 없습니다."),
 	INVALID_INPUT(HttpStatus.BAD_REQUEST, "C-003", "유효하지 않은 입력값입니다."),
-	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, ".C-004", "서버 오류가 발생했습니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "C-004", "서버 오류가 발생했습니다."),
 	UNAUTHORIZED_EXCEPTION(HttpStatus.UNAUTHORIZED, "C-005", "승인되지 않은 요청입니다."),
 	ACCESS_DENIED(HttpStatus.FORBIDDEN, "C-006", "접근이 거부되었습니다."),
 
@@ -32,7 +32,6 @@ public enum ErrorCode {
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "U-001", "유저를 찾을 수 없습니다."),
 	NICKNAME_NOT_BLANK(HttpStatus.BAD_REQUEST, "U-002", "닉네임은 필수 입력값입니다."),
 	NICKNAME_INVALID_FORMAT(HttpStatus.BAD_REQUEST, "U-003", "닉네임 형식이 올바르지 않습니다."),
-	REFRESH_TOKEN_NOT_MATCH(HttpStatus.BAD_REQUEST, "A-006", "리프레시 토큰이 일치하지 않습니다."),
 
 	/**
 	 * Auth/JWT Error
@@ -42,6 +41,7 @@ public enum ErrorCode {
 	TOKEN_EXPIRED_EXCEPTION(HttpStatus.UNAUTHORIZED, "A-003", "만료된 토큰입니다."),
 	TOKEN_BLACKLISTED_EXCEPTION(HttpStatus.UNAUTHORIZED, "A-004", "블랙리스트에 등록된 토큰입니다."),
 	TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "A-005", "요청으로부터 토큰을 찾지 못했습니다."),
+	REFRESH_TOKEN_NOT_MATCH(HttpStatus.BAD_REQUEST, "A-006", "리프레시 토큰이 일치하지 않습니다."),
 
 	/**
 	 * Menu Error
