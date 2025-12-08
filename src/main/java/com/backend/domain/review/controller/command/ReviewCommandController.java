e4d8cf947af95931cfd23a2c71b54da07da141df
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-08 15:58:02 +0900

    ✨ feat(#13): Review API 컨트롤러 구현

---
+++ src/main/java/com/backend/domain/review/controller/command/ReviewCommandController.java
@@ -0,0 +1,68 @@
+package com.backend.domain.review.controller.command;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.common.annotation.CurrentUser;
+import com.backend.common.auth.principal.AuthenticatedUser;
+import com.backend.common.response.BaseResponse;
+import com.backend.common.util.ResponseUtils;
+import com.backend.domain.review.dto.request.ReviewReqDto;
+import com.backend.domain.review.dto.response.ReportResDto;
+import com.backend.domain.review.dto.response.ReviewedResDto;
+import com.backend.domain.review.service.facade.ReviewFacadeService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Review", description = "리뷰 관리 API")
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewCommandController {
+
+	private final ReviewFacadeService reviewFacadeService;
+
+	@PostMapping
+	public ResponseEntity<BaseResponse<ReviewedResDto>> saveReview(
+		@CurrentUser AuthenticatedUser token,
+		@RequestBody @Valid ReviewReqDto reqDto
+	) {
+		return ResponseUtils.ok(reviewFacadeService.createReview(token.getUser(), reqDto));
+	}
+
+	@PatchMapping("/{reviewId}")
+	public ResponseEntity<BaseResponse<ReviewedResDto>> updatedReview(
+		@PathVariable Long reviewId,
+		@CurrentUser AuthenticatedUser token,
+		@RequestBody @Valid ReviewReqDto reqDto
+	) {
+		return ResponseUtils.ok(reviewFacadeService.updateReview(reviewId, token.getUser(), reqDto));
+	}
+
+	@DeleteMapping("/{reviewId}")
+	public ResponseEntity<BaseResponse<Void>> deleteReview(
+		@PathVariable Long reviewId,
+		@CurrentUser AuthenticatedUser token
+	) {
+		reviewFacadeService.deleteReview(reviewId, token.getUser());
+		return ResponseUtils.noContent();
+	}
+
+	//TODO Role_OWNER만 동작할 수 있도록 정의 필요
+	@PostMapping("/{reviewId}/report")
+	public ResponseEntity<BaseResponse<ReportResDto>> reportReview(
+		@PathVariable Long reviewId,
+		@CurrentUser AuthenticatedUser token
+	) {
+		return ResponseUtils.ok(reviewFacadeService.reportReview(reviewId, token.getUser()));
+	}
+}
---
+++ src/main/java/com/backend/domain/review/controller/query/ReviewQueryController.java
@@ -0,0 +1,35 @@
+package com.backend.domain.review.controller.query;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.common.annotation.CurrentUser;
+import com.backend.common.auth.principal.AuthenticatedUser;
+import com.backend.common.response.BaseResponse;
+import com.backend.common.util.ResponseUtils;
+import com.backend.domain.review.dto.response.ReviewedResDto;
+import com.backend.domain.review.service.facade.ReviewFacadeService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Review", description = "리뷰 관리 API")
+@RestController
+@RequestMapping("/reviews")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewQueryController {
+
+	private final ReviewFacadeService reviewFacadeService;
+
+	@GetMapping("{reviewId}")
+	public ResponseEntity<BaseResponse<ReviewedResDto>> getReviewDetails(
+		@PathVariable Long reviewId,
+		@CurrentUser AuthenticatedUser token
+	) {
+		return ResponseUtils.ok(reviewFacadeService.getReviewDetails(reviewId, token.getUser()));
+	}
+}
