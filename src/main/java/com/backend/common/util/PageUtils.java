85d27b146e9182f5aaf1e748b02589a02d3efc9e
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-09 14:51:26 +0900

    ✨ feat(#13): 리스트를 Page 객체로 변환하는 유틸 메서드 추가

--- src/main/java/com/backend/common/util/PageUtils.java
+++ src/main/java/com/backend/common/util/PageUtils.java
@@ -1,5 +1,9 @@
 package com.backend.common.util;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
@@ -10,5 +14,9 @@ public class PageUtils {
 
 	public Pageable getPageable(final int page, final int size) {
 		return PageRequest.of(page - 1, size);
+	}
+
+	public <T> Page<T> toPage(final List<T> content, final Pageable pageable, final long total) {
+		return new PageImpl<>(content, pageable, total);
 	}
 }
