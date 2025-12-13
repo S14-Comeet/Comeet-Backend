02efceba98015847d7c093c9dac37076943d3734
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-14 02:54:42 +0900

    ♻️ refactor(#22): CuppingNoteCommandMapper 파라미터 어노테이션 제거

--- src/main/java/com/backend/domain/review/mapper/command/CuppingNoteCommandMapper.java
+++ src/main/java/com/backend/domain/review/mapper/command/CuppingNoteCommandMapper.java
@@ -1,13 +1,12 @@
 package com.backend.domain.review.mapper.command;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import com.backend.domain.review.entity.CuppingNote;
 
 @Mapper
 public interface CuppingNoteCommandMapper {
-	int insert(@Param("cuppingNote") CuppingNote cuppingNote);
+	int insert(CuppingNote cuppingNote);
 
-	int update(@Param("cuppingNote") CuppingNote cuppingNote);
+	int update(CuppingNote cuppingNote);
 }
