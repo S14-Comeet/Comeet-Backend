0de0a4ba6fbb9cd0a6df0387f72e5371a9e8e0a6
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-15 14:54:07 +0900

    ✨ feat(#33): Bean-Flavor 중간 테이블 매퍼 및 MyBatis 매핑 파일 추가

---
+++ src/main/java/com/backend/domain/bean/mapper/BeanFlavorMapper.java
@@ -0,0 +1,19 @@
+package com.backend.domain.bean.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.backend.domain.flavor.entity.Flavor;
+
+@Mapper
+public interface BeanFlavorMapper {
+	int insertBeanFlavors(@Param("beanId") Long beanId, @Param("flavorIds") List<Long> flavorIds);
+
+	int deleteBeanFlavors(Long beanId);
+
+	List<Flavor> findFlavorsByBeanId(Long beanId);
+
+	List<Flavor> findFlavorsByBeanIds(@Param("beanIds") List<Long> beanIds);
+}
