324e39041721505a6ba4c2399bea19f2083eebae
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-17 16:28:46 +0900

    ♻️ refactor(#38): 매장/로스터리 관리자 역할 MANAGER로 통합

--- src/main/java/com/backend/domain/user/entity/Role.java
+++ src/main/java/com/backend/domain/user/entity/Role.java
@@ -8,8 +8,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Role {
 	USER("ROLE_USER", "사용자", false),
-	STORE_MANAGER("ROLE_STORE_MANAGER", "매장 관리자", true),
-	ROASTERY_MANAGER("ROLE_ROASTERY_MANAGER", "로스터리 관리자", true),
+	MANAGER("ROLE_MANAGER", "관리자", true),
 	GUEST("ROLE_GUEST", "임시 사용자", false),
 	WITHDRAWN("ROLE_WITHDRAWN", "탈퇴한 사용자", false),
 	;
