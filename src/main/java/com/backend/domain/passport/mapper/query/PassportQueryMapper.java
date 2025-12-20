6747a97b9fff714343fe6b8f7ad6642ab18f478f
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-20 17:15:37 +0900

    ✨ feat(#46): 여권 쿼리 매퍼 및 XML에 월별 방문/메뉴 조회 쿼리 추가

--- src/main/java/com/backend/domain/passport/mapper/query/PassportQueryMapper.java
+++ src/main/java/com/backend/domain/passport/mapper/query/PassportQueryMapper.java
@@ -1,7 +1,10 @@
 package com.backend.domain.passport.mapper.query;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -13,4 +16,19 @@ public interface PassportQueryMapper {
 	Passport findById(@Param("passportId") Long passportId);
 
 	List<Passport> findAllByUserIdAndYear(@Param("userId") Long userId, @Param("year") Integer year);
+
+	List<Long> findUsersWithVisitsInMonth(@Param("year") int year, @Param("month") int month);
+
+	Optional<Passport> findByUserIdAndYearAndMonth(
+		@Param("userId") Long userId,
+		@Param("year") int year,
+		@Param("month") int month
+	);
+
+	@MapKey("")
+	List<Map<String, Object>> findVisitsWithMenuInMonth(
+		@Param("userId") Long userId,
+		@Param("year") int year,
+		@Param("month") int month
+	);
 }
