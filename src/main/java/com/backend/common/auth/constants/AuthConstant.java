9c7a68a99f10d8f3594f08f020cb478b87bdba33
윤섭 <54024213+F-UMI@users.noreply.github.com>
2025-12-21 19:32:23 +0900

    🔧 chore(#51): OAuth 리다이렉트 URI 환경변수로 분리 및 설정 추가

--- src/main/java/com/backend/common/auth/constants/AuthConstant.java
+++ src/main/java/com/backend/common/auth/constants/AuthConstant.java
@@ -7,5 +7,4 @@ import lombok.NoArgsConstructor;
 public final class AuthConstant {
 	public static final String AUTHORIZATION = "Authorization";
 	public static final String BEARER = "Bearer ";
-	public static final String LOCAL_OAUTH_REDIRECT_URI = "http://localhost:5173/oauth/callback";
 }
